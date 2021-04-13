f5ea2c8b1f9f4361d4487e140486d7380f308756
Denis Sosnin <sosnin.11.08@gmail.com>
2021-04-13 20:05:51 +0300

    two commit

--- script.js
+++ script.js
@@ -33,3 +33,59 @@ let tree = {
     }
 };
 
+threeword(tree);
+
+function threeword(obj, way) {
+    if (isNode(obj)) {
+        if (typeof way == 'undefined')
+            way = ''
+        if (way != '')
+            way = way + '->';
+
+        threeword(obj.left, way + 'Left');
+        threeword(obj.right, way + 'Right');
+    } else if (isList(obj)) {
+        console.log(way + ': ' + findLongWords(obj, 3))
+    }
+    return null;
+}
+
+function isList(obj) {
+    if (typeof (obj) == "string")
+        return true;
+    else
+        return false;
+}
+
+function isNode(obj) {
+    if (typeof (obj) == "string")
+        return false;
+    else
+        return true;
+}
+
+
+function longer(champ, contender) {
+    return (contender.length > champ.length) ? contender : champ;
+}
+
+function findLongWords(str, word_count) {
+
+    str = str.replace(/[.,!?;|<>]+/g, '');
+
+    let words = str.split(' ');
+    let result = '';
+    for (let i = 1; i <= word_count; i++) {
+        if ((words.length === 0) || ((words.length === 1) && (words[0] === '')))
+            continue;
+
+        let theLongestWord = words.reduce(longer);
+        result = result + theLongestWord + ' ';
+
+        let word = words.indexOf(theLongestWord);
+        if (word != -1) {
+            words.splice(word, 1);
+        }
+    }
+    return result;
+}
